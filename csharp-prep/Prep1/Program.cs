6fe1ccd2073dc3101e5f65940a24888452f10aa7
rorellana-b <ronalorellanarbo@gmail.com>
2024-01-12 23:22:56 -0600

    Finished prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Wha is your first name? ");
+        String first = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        String last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}");
+
+
     }
 }
